174805c17bdaadae004c459b6e4d24572f33051e
Dima Kogan <dima@secretsauce.net>
2012-10-07 12:10:02 -0700

    added project emacs style

---
+++ .dir-locals.el
@@ -0,0 +1,10 @@
+((c-mode . (
+            (c-file-style     . "gnu")
+            (indent-tabs-mode . t)
+            (c-basic-offset   . 4)
+            (tab-width        . 4)
+            (comment-start    . "/*")
+            (comment-end      . "*/")
+            )))
+
+(provide '.dir-locals)
